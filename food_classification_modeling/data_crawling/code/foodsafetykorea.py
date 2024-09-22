f47322b2a34451f2037d9b8f2842fb7070e391e8
조유경 <yugyeongjo1215@gmail.com>
2024-09-23 00:43:50 +0900

    data_scrapping

--- food_classification_modeling/data_crawling/code/foodsafetykorea.py
+++ food_classification_modeling/data_crawling/code/foodsafetykorea.py
@@ -31,16 +31,23 @@ collection.delete_many({})
 
 # 웹 페이지 열기  
 # 크롤링할 웹 페이지 URL
-
 url = f"https://www.foodsafetykorea.go.kr/portal/healthyfoodlife/searchHomeHF.do?menu_grp=MENU_NEW01&menu_no=2823"
 html_source = browser.get(url)
 
+# # 50개씩 보기
+# browser.find_element(By.CSS_SELECTOR,value="#show_cnt > option:nth-child(5)").click()
+# browser.find_element(By.CSS_SELECTOR,value="#show_cnt").click()
 time.sleep(3)
-browser.find_element(By.CSS_SELECTOR,value="#contents > main > div.list-container > table > tbody > tr:nth-child(1) > td:nth-child(2)").click()
 
+# 페이지네이션 넘기기
 while True:
-    time.sleep(1)
-    items_name = browser.find_element(By.CSS_SELECTOR,value="article > table > tbody > tr:nth-child(2) > td").text
-    nutritions = browser.find_element(By.CSS_SELECTOR,value="article > table > tbody > tr:nth-child(13) > td").text
-    collection.insert_one({"items_name": items_name, "nutritions": nutritions})
-    browser.find_element(By.CSS_SELECTOR, value="#contents > main > div.page-container > article > div.board-footer > div.prev-btn-wrap > a:nth-child(2)").click()
+    # 상세보기 들어가기
+    item_names = browser.find_elements(By.CSS_SELECTOR,value="tr > td:nth-child(2) > span.table_txt")
+    for item_name in item_names:
+        item_name.click()
+        time.sleep(3)
+        items_name = browser.find_element(By.CSS_SELECTOR,value="article > table > tbody > tr:nth-child(2) > td").text
+        nutritions = browser.find_element(By.CSS_SELECTOR,value="article > table > tbody > tr:nth-child(13) > td").text
+        collection.insert_one({"items_name": items_name, "nutritions": nutritions})
+        browser.back()
+    browser.find_element(By.CSS_SELECTOR,value="#contents > main > div.list-container > div.board-footer > div > ul > li:nth-child(7) > a").click()
