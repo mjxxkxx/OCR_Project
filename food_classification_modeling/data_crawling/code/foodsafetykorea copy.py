196dca214b5202e2eed8cc2dc86abfa8f71c3770
조유경 <yugyeongjo1215@gmail.com>
2024-09-25 01:04:55 +0900

    crawling

--- food_classification_modeling/data_crawling/code/foodsafetykorea copy.py
+++ food_classification_modeling/data_crawling/code/foodsafetykorea copy.py
@@ -4,37 +4,44 @@ from playwright.async_api import async_playwright
 import requests
 from bs4 import BeautifulSoup
 
-async def foodsafety(page):
-    # 페이지가 로드되고 특정 테이블 요소가 나타날 때까지 대기
-    await page.wait_for_selector("#contents > main > div.list-container > table > tbody > tr > td:nth-child(2)")
-    
-    # 병렬로 데이터를 가져오기 위해 gather 사용
-    item_names_task = page.locator("#contents > main > div.list-container > table > tbody > tr > td:nth-child(2)").all_text_contents()
-    company_names_task = page.locator("#contents > main > div.list-container > table > tbody > tr > td:nth-child(3)").all_text_contents()
-    report_number_task = page.locator("#contents > main > div.list-container > table > tbody > tr > td:nth-child(4)").all_text_contents()
-    
-    # 병렬로 비동기 작업 실행
-    item_names, company_names, report_number = await asyncio.gather(
-        item_names_task, company_names_task, report_number_task
-    )
-    
-    # 슬라이싱
-    item_names = [name[3:] for name in item_names]
-    company_names = [name[3:] for name in company_names]
-    report_number = [number[4:] for number in report_number]
-    
-    # 딕셔너리로 데이터 저장
-    data_dict = {
-        "item_names": item_names,
-        "company_names": company_names,
-        "report_number": report_number
-    }
+def foodsafety():
+    """This function extracts values for each product
+    """
+    url = 'https://www.foodsafetykorea.go.kr/portal/healthyfoodlife/searchHomeHF.do?menu_grp=MENU_NEW01&amp;menu_no=2823'
+    data_dict = {'item_values': []} 
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        tbody = soup.find('tbody')
+        if tbody:
+            hidden_inputs = tbody.find_all('input', {'type': 'hidden'})
+
+            for input_tag in hidden_inputs:
+                value = input_tag.get('value')
+                if value:
+                    data_dict['item_values'].append(value)  # 리스트에 값 추가
+        else:
+            print("No tbody found on the page")
+    else:
+        print("Failed to retrieve the page")
 
     return data_dict
+#############
+질문내용
+1) ctrl + U = requests
+2) 개발자도구 = BeautifulSoup
+3) 매크로 = playwright
+4) async = 1+2+3 모두 비동기로 작동
+이거 맞나요...?
+구분이 잘 안가네요...
+#############
 
 # 상세 페이지 요청을 위한 함수
-async def crawl_details(report_numbers):
-    url = 'https://www.foodsafetykorea.go.kr/portal/healthyfoodlife/searchHomeHFDetail.do'
+async def crawl_details(item_values):
+    url = f'https://www.foodsafetykorea.go.kr/portal/healthyfoodlife/searchHomeHFDetail.do?prdlstReportLedgNo={item_values}&amp;search_code=01&amp;start_idx=1&amp;show_cnt=10&amp;menu_no=2823&amp;menu_grp=MENU_NEW01'
 
     # 비동기 HTTP 세션을 생성하여 여러 요청을 병렬로 실행
     async with aiohttp.ClientSession() as session:
@@ -58,27 +65,6 @@ async def crawl_details(report_numbers):
 
 # 세부 페이지를 요청하는 비동기 함수
 async def fetch_detail_page(session, url):
-    # params = {'prdlstReportLedgNo': '2024021000386672'}  # 신고번호를 파라미터로 전달
-
-    # # async with session.get(url, params=params) as response:
-    # headers = {
-    #     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    #     "accept-encoding": "gzip, deflate, br, zstd",
-    #     "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-    #     "cache-control": "max-age=0",
-    #     "connection": "keep-alive",
-    #     "content-type": "application/x-www-form-urlencoded",
-    #     "sec-ch-ua": '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
-    #     "sec-ch-ua-mobile": "?0",
-    #     "sec-ch-ua-platform": "Windows",
-    #     "sec-fetch-dest": "document",
-    #     "sec-fetch-mode": "navigate",
-    #     "sec-fetch-site": "same-origin",
-    #     "sec-fetch-user": "?1",
-    #     "upgrade-insecure-requests": "1",
-    #     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
-    # }
-
     async with session.get(url) as response:
         print('start crawling from site')
         text = await response.text()
@@ -88,23 +74,20 @@ async def fetch_detail_page(session, url):
 
 # main 함수
 async def main():
-    async with aiohttp.ClientSession() as session:
-        await fetch_detail_page(session, 'https://www.foodsafetykorea.go.kr:443/portal/healthyfoodlife/searchHomeHFDetail.do?prdlstReportLedgNo=2024021000386672&amp;search_code=01&amp;start_idx=1&amp;show_cnt=10&amp;menu_no=2823&amp;menu_grp=MENU_NEW01')
-    # async with async_playwright() as p:
-    #     browser = await p.chromium.launch(headless=False)
-    #     page = await browser.new_page()
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        page = await browser.new_page()
 
-    #     # 첫 번째 페이지에서 기본 정보 가져오기
-    #     await page.goto('https://www.foodsafetykorea.go.kr/portal/healthyfoodlife/searchHomeHF.do?menu_grp=MENU_NEW01&menu_no=2823')
-    #     result = await foodsafety(page)
+        # 첫 번째 페이지에서 value 가져오기
+        await page.goto('https://www.foodsafetykorea.go.kr/portal/healthyfoodlife/searchHomeHF.do?menu_grp=MENU_NEW01&menu_no=2823')
+        result = await foodsafety(page)
 
-    #     # 신고번호 기반으로 상세 페이지 크롤링
-    #     await crawl_details(result['report_number'])
+        # value 기반으로 상세 페이지 크롤링
+        await crawl_details(result['item_values'])
 
-    #     # 브라우저 닫기
-    #     await browser.close()
+        # 브라우저 닫기
+        await browser.close()
 
 if __name__ == '__main__':
-    asyncio.run(main())
-    
-## 신고번호가 아니라 input의 value값으로 새로 추출 후 다시 크롤링 진행 해야함
+    print(foodsafety())
+    # asyncio.run(main())
